c6808e88545215d42fff01b134789114bd7ebfe2
Coding Made Easy <110712694+codingmadeasy@users.noreply.github.com>
2025-06-25 11:58:48 +0530

    Update ch1qd.c

--- ch1qd.c
+++ ch1qd.c
@@ -1,3 +1,5 @@
+/*Q: (d) Temperature of a city in Fahrenheit degrees is input through the keyboard.
+    Write a program to convert this temperature into Centigrade degrees.*/
 #include <stdio.h>
 int main()
 {
